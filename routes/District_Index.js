a63bb190308bca8f74bdb5fc207b2054d7a3d71a
WakisaC <wakisag@gmail.com>
2018-10-02 12:04:29 -0700

    First commit

---
+++ routes/District_Index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('District/index', function(req, res, next) {
+    res.render('index', {page:'Home', menuId:'home'});
+});
+
+router.get('/reports', function(req, res, next) {
+    res.render('reports', {page:'Reports', menuId:'reports'});
+});
+
+router.get('/statistics', function(req, res, next) {
+    res.render('statistics', {page:'Statistics', menuId:'Available Statistics'});
+});
+
+module.exports = router;
